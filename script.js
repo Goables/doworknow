7549772f44932215d209996f4f87e4e69c54cc08
Fraggleblap <Owen.Gaffney23@student.cart.org>
2024-05-02 15:16:05 -0700

    Initial

---
+++ script.js
@@ -0,0 +1,39 @@
+async function pointsCounter() {
+    let element = document.getElementById('seconds');
+    let mElement = document.getElementById('minutes');
+    let hElement = document.getElementById('hours');
+    let counter = 0;
+    let mCounter = 0;
+    let hCounter = 0;
+
+    const DEF_DELAY = 1000;
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms || DEF_DELAY));
+    }
+
+    while(true){
+    await sleep(1000);
+    if (counter === 60) {
+        counter = 0;
+        element.innerHTML = '0';
+        mCounter++;
+        if (mCounter === 60) {
+            mCounter = 0;
+            mElement.innerHTML = '0';
+            hCounter++;
+            if (hCounter >= 1) {
+                window.alert(`Ready to do your work?`);
+                window.open(`https://classroom.google.com`);
+            }
+        }
+    }
+    counter++;
+    element.innerHTML = counter;
+    mElement.innerHTML = mCounter;
+    hElement.innerHTML = hCounter;
+    }
+
+}
+
+// WE'RE BORROWING IT MR FAST PLS DONT REMOVE MY PORFOESOINAL POIONRTS 🙏
